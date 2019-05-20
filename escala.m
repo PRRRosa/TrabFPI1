d26540683db66c2a53ab6d6f567df983631055a8
Caio Roberto <caiorrs@gmail.com>
2019-05-19 21:33:11 -0300

    modificação final

--- escala.m
+++ escala.m
@@ -3,13 +3,10 @@
 % escala_desejada é um número
 % se escala_desejada > 1, aumenta a imagem
 % se escala_desejada < 1, diminui a imagem
-% A imagem deve ser do tamanho 512x512 - seguimos a resolução da lenna original
+
 function escala(in_image, scale)
-
     [~, FileName, ~] = fileparts(in_image);
-    
     in_image = imread(in_image);
-
     figure, imshow(in_image), title('Imagem original');
     
     red = in_image(:,:,1);
@@ -17,11 +14,9 @@ function escala(in_image, scale)
     blue = in_image(:,:,3);
     
     % coordenadas originais
-
     [x,y] = meshgrid(1:512, 1:512);
 
     % novas coordenadas
-
     a = scale;
     if a > 0
         for i=1:512
@@ -30,19 +25,14 @@ function escala(in_image, scale)
                 y1(i,j) = double(y(i,j)/a);
             end
         end
-
         % faz a interpolação
-        %tic
         in_image_red_c = interp2(double(x), double(y), double(red), x1, y1, 'cubic');
         in_image_green_c = interp2(double(x), double(y), double(green), x1, y1, 'cubic');
         in_image_blue_c = interp2(double(x), double(y), double(blue), x1, y1, 'cubic');
-        %toc
 
-        %tic
         in_image_red_n = interp2(double(x), double(y), double(red), x1, y1, 'nearest');
         in_image_green_n = interp2(double(x), double(y), double(green), x1, y1, 'nearest');
         in_image_blue_n = interp2(double(x), double(y), double(blue), x1, y1, 'nearest');
-        %toc
 
         out_image_c = cat(3, in_image_red_c, in_image_green_c, in_image_blue_c);
         out_image_n = cat(3, in_image_red_n, in_image_green_n, in_image_blue_n);
@@ -55,5 +45,5 @@ function escala(in_image, scale)
 
         figure, imshow(uint8(out_image_c)), title('Cúbica');
         figure, imshow(uint8(out_image_n)), title('Vizinho Mais Próximo');
-    end
+     end
 end
